f4d4a29bbf883f3a57feb10faf26b1172ea4099b
Muhammad zaid <98674411+mdzaid31@users.noreply.github.com>
2024-04-25 21:45:57 +0400

    first commit

---
+++ constants/index.ts
@@ -0,0 +1,93 @@
+export const Skill_data = [
+    {
+      skill_name: "Html 5",
+      Image: "/html.png",
+      width: 80,
+      height: 80,
+    },
+    {
+      skill_name: "Css",
+      Image: "/css.png",
+      width: 80,
+      height: 80,
+    },
+    {
+      skill_name: "Java Script",
+      Image: "/js.png",
+      width: 65,
+      height: 65,
+    },
+    {
+      skill_name: "Tailwind Css",
+      Image: "/tailwind.png",
+      width: 80,
+      height: 80,
+    },
+    {
+      skill_name: "React",
+      Image: "/react.png",
+      width: 80,
+      height: 80,
+    },
+    
+  
+  ];
+  
+  export const Socials = [
+    {
+      name: "Discord",
+      src: "/discord.svg",
+    },
+    {
+      name: "Facebook",
+      src: "/facebook.svg",
+    },
+    {
+      name: "Instagram",
+      src: "/instagram.svg",
+    },
+  ];
+  
+  
+  
+  
+  export const Backend_skill = [
+    
+    {
+      skill_name: "Postger SQL",
+      Image: "/postger.png",
+      width: 70,
+      height: 70,
+    },
+    {
+      skill_name: "My SQL",
+      Image: "/mysql.png",
+      width: 70,
+      height: 70,
+    },
+
+  ];
+  
+  export const Full_stack = [
+    
+    {
+      skill_name: "Docker",
+      Image: "/docker.webp",
+      width: 70,
+      height: 70,
+    },
+  
+    {
+      skill_name: "Figma",
+      Image: "/figma.png",
+      width: 50,
+      height: 50,
+    },
+  
+  ];
+  
+  
+  
+  
+  
+  
