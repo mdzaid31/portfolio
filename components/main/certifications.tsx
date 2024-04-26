16232d9e771a9303ef0af9e7c98f3338d79ae11d
Muhammad zaid <98674411+mdzaid31@users.noreply.github.com>
2024-04-26 15:42:36 +0400

    22nd

--- components/main/certifications.tsx
+++ components/main/certifications.tsx
@@ -10,22 +10,27 @@ const Certifications = () => {
       <h1 className="text-[40px] font-semibold text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500 py-2">
         My Certifications
       </h1>
-      <div className="h-full w-full flex flex-col md:flex-row gap-10 px-10">
-        <ProjectCard
-          src="/sample.png"
-          title="Samsung Innovation Campus UAE AI Certification"
-          description="AI, Machine Learning, and Deep Learning Course.
-          Statistics, Probability, Calculus, Data Analysis,
-          Preprocessing and Visualization"
-        />
-        <ProjectCard
-          src="/sample.png"
-          title="Shaw Academy - Web development"
-          description="Front End Web Development Course
-          HTML, CSS, JS
-          Made an online grocery webpage concept that lets you
-          order and outputs your total price."
-        />
+      <div
+        className="h-full flex flex-col md:flex-row gap-10 px-10"
+        style={{ width: "500px" }}
+      >
+        <center>
+          <ProjectCard
+            src="/sample.png"
+            title="Samsung Innovation Campus UAE AI Certification"
+            description="AI, Machine Learning, and Deep Learning Course.
+            Statistics, Probability, Calculus, Data Analysis,
+            Preprocessing and Visualization"
+          />
+          <ProjectCard
+            src="/sample.png"
+            title="Shaw Academy - Web development"
+            description="Front End Web Development Course
+            HTML, CSS, JS
+            Made an online grocery webpage concept that lets you
+            order and outputs your total price."
+          />
+        </center>
       </div>
       <br />
     </div>
